7b37741dc2fd23260e86ad04141b7ab1b97bbe7f
Jesús Morán Espinoza <jame291104@gmail.com>
2025-01-29 21:46:37 -0500

    config para digital ocean y agregar front al proyecto

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import { connectionMongo } from './src/config/dataBase.js';
 import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
 import cors from 'cors'; 
+// importaciones para acceder a las rutas del front desde el back
+import path from "path";
+import { fileURLToPath } from "url";
 
 // 2. configurar el uso de nuestro servidor y dependencias
 const app = express(); 
@@ -13,12 +16,21 @@ dotenv.config();
 connectionMongo();
 app.use(cors()); 
 
+//Confirguraciones para acceder al frontend
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Le indico las rutas que debe utilizar
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
-app.get('/', (req, res) => {
-    res.send('El Backend Esta Melo Caramelo');
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 export default app;
